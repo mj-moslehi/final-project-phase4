d4346719ab636813adf2a1a3cea11d4821649b3f
mamad <you@example.com>
2024-07-19 08:07:26 +0330

    create ConfirmationTokenService class

---
+++ src/main/java/ir/moslehi/finalprojectphase4/service/ConfirmationTokenService.java
@@ -0,0 +1,29 @@
+package ir.moslehi.finalprojectphase4.service;
+
+import ir.moslehi.finalprojectphase4.model.ConfirmationToken;
+import ir.moslehi.finalprojectphase4.repository.ConfirmationTokenRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
+
+@Service
+@AllArgsConstructor
+public class ConfirmationTokenService {
+
+    private final ConfirmationTokenRepository confirmationTokenRepository;
+
+    public void saveConfirmationToken(ConfirmationToken token) {
+        confirmationTokenRepository.save(token);
+    }
+
+    public Optional<ConfirmationToken> getToken(String token) {
+        return confirmationTokenRepository.findByToken(token);
+    }
+
+    public void setConfirmedAt(String token) {
+        confirmationTokenRepository.updateConfirmedAt(
+                token, LocalDateTime.now());
+    }
+}
