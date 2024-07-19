8f6c305652f6f6e435f0495e51fe906f69818ff5
mamad <you@example.com>
2024-07-19 08:00:44 +0330

    create AdminService class

---
+++ src/main/java/ir/moslehi/finalprojectphase4/service/AdminService.java
@@ -0,0 +1,27 @@
+package ir.moslehi.finalprojectphase4.service;
+
+import ir.moslehi.finalprojectphase4.exception.DuplicateInformationException;
+import ir.moslehi.finalprojectphase4.model.Admin;
+import ir.moslehi.finalprojectphase4.model.enums.Role;
+import ir.moslehi.finalprojectphase4.repository.AdminRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AdminService {
+
+    private final AdminRepository adminRepository;
+    private final BCryptPasswordEncoder passwordEncoder;
+
+    public Admin save(Admin admin) {
+        if (adminRepository.findByEmail(admin.getEmail()).isPresent())
+            throw new DuplicateInformationException(admin.getEmail() + " is duplicate");
+        admin.setPassword(passwordEncoder.encode(admin.getPassword()));
+        admin.setRole(Role.ROLE_ADMIN);
+        admin.setEnabled(true);
+        return adminRepository.save(admin);
+    }
+
+}
