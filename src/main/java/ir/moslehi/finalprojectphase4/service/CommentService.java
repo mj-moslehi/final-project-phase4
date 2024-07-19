317d272aab4720826ee307d7aac74f2880ab9e23
mamad <you@example.com>
2024-07-19 08:06:10 +0330

    create CommentService class

---
+++ src/main/java/ir/moslehi/finalprojectphase4/service/CommentService.java
@@ -0,0 +1,43 @@
+package ir.moslehi.finalprojectphase4.service;
+
+import ir.moslehi.finalprojectphase4.exception.DuplicateInformationException;
+import ir.moslehi.finalprojectphase4.exception.NotFoundException;
+import ir.moslehi.finalprojectphase4.exception.NotValidInput;
+import ir.moslehi.finalprojectphase4.model.*;
+import ir.moslehi.finalprojectphase4.model.enums.OrderStatus;
+import ir.moslehi.finalprojectphase4.repository.CommentRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class CommentService {
+
+    private final CommentRepository commentRepository;
+    private final SuggestionService suggestionService;
+    private final CustomerService customerService;
+    private final ExpertService expertService;
+
+    public Comment save(Comment comment) {
+        Customer customer = customerService.findById(comment.getCustomer().getId());
+        Expert expert = expertService.findById(comment.getExpert().getId());
+        Orders orders = suggestionService.
+                validOrderForCustomerWithOrderStatus(OrderStatus.DONE, customer, comment.getOrders().getId());
+        if (!orders.getExpert().equals(expert))
+            throw new NotValidInput("the expert id isn't valid for this order");
+        if (commentRepository.findByOrders(orders).isPresent())
+            throw new DuplicateInformationException("this comment for this order have been saved");
+        comment.setExpert(expert);
+        comment.setCustomer(customer);
+        return commentRepository.save(comment);
+    }
+
+    public List<Comment> findByExpert(Expert expert) {
+        if (commentRepository.findByExpert(expert).isEmpty())
+            throw new NotFoundException("there isn't any comment for expert by Id : "+expert.getId());
+        return commentRepository.findByExpert(expert);
+    }
+
+}
