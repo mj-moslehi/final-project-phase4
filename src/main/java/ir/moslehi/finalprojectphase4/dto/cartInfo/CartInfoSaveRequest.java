99ee32a9c05bbd361b363cbed704e16ef00e23bf
mamad <you@example.com>
2024-07-19 08:03:43 +0330

    create CartInfoSaveRequest record

---
+++ src/main/java/ir/moslehi/finalprojectphase4/dto/cartInfo/CartInfoSaveRequest.java
@@ -0,0 +1,22 @@
+package ir.moslehi.finalprojectphase4.dto.cartInfo;
+
+import ir.moslehi.finalprojectphase4.dto.customer.CustomerIdRequest;
+import jakarta.validation.constraints.Pattern;
+
+public record CartInfoSaveRequest(
+
+        @Pattern(regexp = "^[0-9]{16}$")
+        String cartNumber,
+
+        @Pattern(regexp = "^[0-9]{4,5}$")
+        String cvv2,
+
+        @Pattern(regexp = "^[0-9]{4,6}$")
+        String password,
+
+        @Pattern(regexp = "^((?:19|20)[0-9][0-9])-(0?[1-9]|1[012])$")
+        String stringDate,
+
+        CustomerIdRequest customer
+) {
+}
