cacc1300025972b77659d22c7729f375cc9f64e5
mamad <you@example.com>
2024-07-19 08:02:46 +0330

    create CartInfo class

---
+++ src/main/java/ir/moslehi/finalprojectphase4/model/CartInfo.java
@@ -0,0 +1,46 @@
+package ir.moslehi.finalprojectphase4.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+import java.util.Date;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@Builder
+
+public class CartInfo  {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+
+    @Column(unique = true)
+    String cartNumber;
+
+    @Column(unique = true)
+    String cvv2;
+
+    String password;
+
+    Date expirationDate;
+
+    @ManyToOne
+    Customer customer;
+
+    @Override
+    public String toString() {
+        return "CartInfo{" +
+                "cartNumber='" + cartNumber + '\'' +
+                ", cvv2='" + cvv2 + '\'' +
+                ", password='" + password + '\'' +
+                ", expirationDate=" + expirationDate +
+                ", id=" + id +
+                '}';
+    }
+}
